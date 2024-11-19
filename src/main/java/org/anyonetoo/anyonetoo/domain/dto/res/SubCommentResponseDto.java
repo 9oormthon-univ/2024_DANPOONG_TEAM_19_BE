73928975e395417f13efa8d89c083b607066e213
xunxxoie <ahh0520@naver.com>
2024-11-20 03:30:00 +0900

    [Feat] Comment CRUD middle Commit - #1

--- src/main/java/org/anyonetoo/anyonetoo/domain/dto/res/SubCommentResponseDto.java
+++ src/main/java/org/anyonetoo/anyonetoo/domain/dto/res/SubCommentResponseDto.java
@@ -20,7 +20,7 @@ public class SubCommentResponseDto {
     @Schema(description = "작성자", example = "옥자")
     private String username;
 
-    @Schema(description = "댓글 내용", example = "헐! 사고싶어요!")
+    @Schema(description = "댓글 내용", example = "헐! 사고싶어요")
     private String content;
 
     @Schema(description = "작성일", example = "2024-10-22")
