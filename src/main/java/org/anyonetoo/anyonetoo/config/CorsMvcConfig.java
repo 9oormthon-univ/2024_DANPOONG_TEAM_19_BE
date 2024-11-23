68beeb87eba4daaf74be22b5f0fd11ed22e0ed88
grapyju <yoonju203@naver.com>
2024-11-23 20:50:39 +0900

    [Fix] cors

--- src/main/java/org/anyonetoo/anyonetoo/config/CorsMvcConfig.java
+++ src/main/java/org/anyonetoo/anyonetoo/config/CorsMvcConfig.java
@@ -11,7 +11,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
         corsRegistry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*") // 허용할 HTTP 메서드
                 .allowedHeaders("*"); // 모든 헤더 허용
               //  .allowCredentials(true);
