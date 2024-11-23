02c19f25ceccae0ce6ef88f673d4dd47c4d02e84
xunxxoie <ahh0520@naver.com>
2024-11-24 04:19:32 +0900

    [Fix] update alarm logic

--- src/main/java/org/anyonetoo/anyonetoo/controller/AlarmController.java
+++ src/main/java/org/anyonetoo/anyonetoo/controller/AlarmController.java
@@ -1,5 +1,8 @@
 package org.anyonetoo.anyonetoo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.anyonetoo.anyonetoo.domain.dto.alarm.AlarmResponseDto;
 import org.anyonetoo.anyonetoo.domain.dto.alarm.OrderAlarmResponseDto;
@@ -21,6 +24,11 @@ import java.util.List;
 public class AlarmController {
 
     private final AlarmService alarmService;
+    @Operation(summary = "상품 삭제")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "알람 조회 성공"),
+            @ApiResponse(responseCode = "404", description = "유저 조회 실패")
+    })
     @GetMapping
     public ResponseEntity<ResponseDto<List<AlarmResponseDto>>> getAlarms(@AuthenticationPrincipal User user){
      return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.of(alarmService.getAlarms(user), "알람 불러오기 성공"));
